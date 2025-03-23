a59604da4ab9775301792c2b9786471ad624d166
quqxiaoli <y1756062305@gmail.com>
2025-03-23 14:12:55 +0800

    Temporarily disable rate limiting to test connection pooling

--- routes/routes.go
+++ routes/routes.go
@@ -2,12 +2,12 @@ package routes
 
 import (
     "collaborative-blackboard/handlers"
-    "collaborative-blackboard/middleware"
+    //"collaborative-blackboard/middleware"
     "github.com/gin-gonic/gin"
 )
 
 func SetupRouter(r *gin.Engine) {
-    r.Use(middleware.RateLimit(10)) // 每秒最多 10 次请求
+    //r.Use(middleware.RateLimit(10)) // 每秒最多 10 次请求
     r.POST("/register", handlers.Register)
     r.POST("/login", handlers.Login)
     r.POST("/room", handlers.CreateRoom)
